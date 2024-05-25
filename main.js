57222893719e3c830c1bc0d86732f4dad42134c9
Mohammad Khan <qcc2016@yahoo.com>
2024-05-25 17:45:52 -0400

    added functions

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.querySelector("#modal");
 
+  const emptyHearts = document.querySelectorAll(".like-glyph");
+  
+  errorModal.classList.add('hidden');
 
+  // Event listener for clicking on empty hearts
+  emptyHearts.forEach(emptyHeart => {
+    emptyHeart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          emptyHeart.innerText = FULL_HEART;
+          emptyHeart.classList.add("activated-heart");
+        })
+        .catch(error => {
+          errorModal.classList.remove("hidden");
+          errorModal.innerText = error;
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+
+  // Event listener for clicking on full hearts
+  const fullHearts = document.querySelectorAll(".activated-heart");
+  fullHearts.forEach(fullHeart => {
+    fullHeart.addEventListener("click", () => {
+      fullHeart.innerText = EMPTY_HEART;
+      fullHeart.classList.remove("activated-heart");
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
